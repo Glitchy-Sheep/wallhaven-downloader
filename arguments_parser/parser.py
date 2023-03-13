122afccff12eecbe3fb4ad41cff681b00293ed93
Glitchy-Sheep <crazzzy_coder@mail.ru>
2023-03-13 12:27:05 +0300

    Add functions for extraction "purity" and "category" filters

--- arguments_parser/parser.py
+++ arguments_parser/parser.py
@@ -1,5 +1,7 @@
 import argparse
-from .help_messages import *
+
+from arguments_parser.help_messages import *
+from aiowallhaven.wallhaven_types import Purity, Category
 
 
 parser = argparse.ArgumentParser(
@@ -59,4 +61,35 @@ parser.add_argument('--threads', '-t',
                     metavar='count',
                     help=HELP_MSG_THREADS)
 
+
 args = vars(parser.parse_args())
+
+
+def get_purity_filter():
+    if args['purity'] is None:
+        return Purity(True, True, True)
+
+    purity_filter = Purity(False, False, False)
+
+    if "sfw" in args['purity']:
+        purity_filter.sfw = True
+    if "sketchy" in args['purity']:
+        purity_filter.sketchy = True
+    if "nsfw" in args['purity']:
+        purity_filter.nsfw = True
+    return purity_filter
+
+
+def get_category_filter():
+    if args['category'] is None:
+        return Category(True, True, True)
+
+    category_filter = Category(False, False, False)
+
+    if "general" in args['category']:
+        category_filter.general = True
+    if "anime" in args['category']:
+        category_filter.anime = True
+    if "people" in args['category']:
+        category_filter.people = True
+    return category_filter
