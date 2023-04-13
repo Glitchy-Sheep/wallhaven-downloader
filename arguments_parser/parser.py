25c67b54689e22075e068883a57b3e9d79440b84
Glitchy-Sheep <crazzzy_coder@mail.ru>
2023-04-13 16:05:14 +0300

    Move argument names into separate variables.
    Some getter functions uses argument names more than one time,
    so if one changed I had to change others.
    So it's better to keep it in one place.

--- arguments_parser/parser.py
+++ arguments_parser/parser.py
@@ -16,15 +16,26 @@ parser = argparse.ArgumentParser(
     formatter_class=argparse.RawTextHelpFormatter,
     description=help_messages.PROGRAM_DESCRIPTION)
 
+# ------------- Arguments name for convenience
+info_arg_name = "info"
+collections_arg_name = "collections"
+uploads_arg_name = "uploads"
+purity_arg_name = "purity"
+category_arg_name = "category"
+sync_arg_name = "sync"
+downloads_path_arg_name = "downloads-path"
+verbose_arg_name = "verbose"
+workers_arg_name = "workers"
 
-parser.add_argument('--info', '-i',
+
+parser.add_argument(f'--{info_arg_name}', f'-{info_arg_name[0]}',
                     required=False,
                     type=str,
                     nargs='+',
                     metavar='',
                     help=help_messages.HELP_MSG_INFO)
 
-parser.add_argument('--collections', '-c',
+parser.add_argument(f'--{collections_arg_name}', f'-{collections_arg_name[0]}',
                     required=False,
                     type=str,
                     nargs='+',
@@ -32,46 +43,46 @@ parser.add_argument('--collections', '-c',
                     metavar='',
                     help=help_messages.HELP_MSG_COLLECTIONS)
 
-parser.add_argument('--uploads', '-u',
+parser.add_argument(f'--{uploads_arg_name}', f'-{uploads_arg_name[0]}',
                     required=False,
                     type=str,
                     nargs='+',
                     metavar='',
                     help=help_messages.HELP_MSG_UPLOADS)
 
-parser.add_argument('--purity',
+parser.add_argument(f'--{purity_arg_name}',
                     required=False,
                     type=str,
                     nargs='+',
                     metavar='',
                     help=help_messages.HELP_MSG_PURITY)
 
-parser.add_argument('--category',
+parser.add_argument(f'--{category_arg_name}',
                     required=False,
                     type=str,
                     nargs='+',
                     metavar='',
                     help=help_messages.HELP_MSG_CATEGORY)
 
-parser.add_argument('--sync', '-s',
+parser.add_argument(f'--{sync_arg_name}', f'-{sync_arg_name[0]}',
                     required=False,
                     action="store_true",
                     help=help_messages.HELP_MSG_SYNC)
 
-parser.add_argument('--downloads-path', '-d',
+parser.add_argument(f'--{downloads_path_arg_name}', f'-{downloads_path_arg_name[0]}',
                     required=False,
                     type=str,
                     metavar='',
                     default=DEFAULT_DOWNLOADS_PATH,
                     help=help_messages.HELP_MSG_DOWNLOADS_PATH)
 
-parser.add_argument('--verbose', '-v',
+parser.add_argument(f'--{verbose_arg_name}', '-v',
                     required=False,
                     action="store_true",
                     default=DEFAULT_VERBOSE,
                     help=help_messages.HELP_MSG_VERBOSE)
 
-parser.add_argument('--threads', '-t',
+parser.add_argument(f'--{workers_arg_name}', f'-{workers_arg_name[0]}',
                     required=False,
                     type=int,
                     metavar='count',
@@ -83,7 +94,7 @@ args = vars(parser.parse_args())
 
 
 def get_info_usernames():
-    usernames = args['info']
+    usernames = args[info_arg_name]
     if usernames is None:
         return []
     else:
@@ -95,16 +106,16 @@ def get_all_tasks() -> list[CollectionTask | UploadTask]:
 
     # each collection is a list with first element as a username
     # and all others as its collections
-    if args['collections']:
-        for collection in args['collections']:
+    if args[collections_arg_name]:
+        for collection in args[collections_arg_name]:
             tasks.append(CollectionTask(
                 username=collection[0],
                 collections=collection[1:],
                 save_directory=COLLECTIONS_PATH + os.sep + collection[0]))
 
     # uploads arg is more simple - it's just a list o usernames
-    if args['uploads']:
-        for upload in args['uploads']:
+    if args[uploads_arg_name]:
+        for upload in args[uploads_arg_name]:
             tasks.append(UploadTask(
                 username=upload,
                 save_directory=UPLOADS_PATH + os.sep + upload))
@@ -113,49 +124,49 @@ def get_all_tasks() -> list[CollectionTask | UploadTask]:
 
 
 def get_purity_filter():
-    if args['purity'] is None:
+    if args[purity_arg_name] is None:
         return Purity(True, True, True)
 
     purity_filter = Purity(False, False, False)
 
-    if "sfw" in args['purity']:
+    if "sfw" in args[purity_arg_name]:
         purity_filter.sfw = True
-    if "sketchy" in args['purity']:
+    if "sketchy" in args[purity_arg_name]:
         purity_filter.sketchy = True
-    if "nsfw" in args['purity']:
+    if "nsfw" in args[purity_arg_name]:
         purity_filter.nsfw = True
     return purity_filter
 
 
 def get_category_filter():
-    if args['category'] is None:
+    if args[category_arg_name] is None:
         return Category(True, True, True)
 
     category_filter = Category(False, False, False)
 
-    if "general" in args['category']:
+    if "general" in args[category_arg_name]:
         category_filter.general = True
-    if "anime" in args['category']:
+    if "anime" in args[category_arg_name]:
         category_filter.anime = True
-    if "people" in args['category']:
+    if "people" in args[category_arg_name]:
         category_filter.people = True
     return category_filter
 
 
 def get_downloads_path():
-    return args['downloads_path']
+    return args[downloads_path_arg_name]
 
 
 def get_logger_level():
-    verbose = args['verbose']
+    verbose = args[verbose_arg_name]
     if verbose:
         return "INFO"
     else:
         return "WARNING"
 
 
-def get_threads_count():
-    threads = args['threads']
+def get_workers_count():
+    threads = args[workers_arg_name]
     if threads <= 0:
         threads = DEFAULT_THREADS_COUNT
     return threads
