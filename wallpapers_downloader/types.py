f75d7ee49b8739cc42101d137a8e9afe2401b822
Roman Berezkin <crazzzy_coder@mail.ru>
2023-07-14 23:16:14 +0300

    Add basic doc strings to downloader.py types, new UserCollection type
    UserCollection type is used instead of dicts, so it's more convenient for coding

--- wallpapers_downloader/types.py
+++ wallpapers_downloader/types.py
@@ -1,8 +1,23 @@
 from dataclasses import dataclass
+from aiowallhaven.wallhaven_types import CollectionInfo
+
+
+@dataclass
+class UserCollections:
+    """
+    Object representing user collections.
+    """
+
+    username: str
+    collections: list[CollectionInfo]
 
 
 @dataclass
 class CollectionTask:
+    """
+    Object representing collection task.
+    """
+
     username: str
     save_directory: str
     collections: list[str]
@@ -10,5 +25,9 @@ class CollectionTask:
 
 @dataclass
 class UploadTask:
+    """
+    Object representing upload task.
+    """
+
     username: str
     save_directory: str
