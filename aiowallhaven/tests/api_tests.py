9a9665431dd69e67dcc51371275a9ec5c320775a
Roman Berezkin <crazzzy_coder@mail.ru>
2023-07-20 21:23:26 +0300

    All tests have been adapted for the new type system
    - And each of them has been successfully passed ✅

--- aiowallhaven/tests/api_tests.py
+++ aiowallhaven/tests/api_tests.py
@@ -4,6 +4,7 @@ import warnings  # disable some socket warnings
 from datetime import datetime as dt  # for "sorting" test
 
 from aiowallhaven.api import WallHavenAPI
+from aiowallhaven.types.wallhaven_enums import Category
 from aiowallhaven.types.wallhaven_types import (
     Ratio,
     Resolution,
@@ -13,6 +14,13 @@ from aiowallhaven.types.wallhaven_types import (
     Color,
     Purity,
     CategoryFilter,
+    SearchFilter,
+    PurityFilter,
+    UserCollectionInfo,
+    WallpaperTag,
+    UserSettings,
+    WallpaperCollection,
+    WallpaperInfo,
 )
 
 API_KEY = os.getenv("WALLHAVEN_API_KEY")
@@ -38,113 +46,113 @@ class ApiTestSearch(unittest.IsolatedAsyncioTestCase):
 
     async def test_query(self):
         target_query = "pool"
-        response = await api.search(q=target_query)
-        query = response["meta"]["query"]
+        response = await api.search(query=target_query)
+        query = response.meta.query
         self.assertEqual(query, target_query)
 
     async def test_categories(self):
-        all_categories = [
-            CategoryFilter(general=True),
-            CategoryFilter(anime=True),
-            CategoryFilter(people=True),
-        ]
+        all_categories = [Category.general, Category.anime, Category.people]
 
-        for category in all_categories:
-            response = await api.search(category=category)
-            wallpapers = response["data"]
-            for wallpaper in wallpapers:
-                self.assertIn(wallpaper["category"], category._get_active_names())
+        for test_category in all_categories:
+            search_filter = SearchFilter(
+                category=CategoryFilter(test_category),
+            )
+
+            response = await api.search(search_filter=search_filter)
+            for wallpaper in response.wallpapers:
+                self.assertEqual(wallpaper.category, test_category)
 
     async def test_purity(self):
-        all_purity = [Purity(sfw=True), Purity(sketchy=True), Purity(nsfw=True)]
+        all_purity = [Purity.sfw, Purity.sketchy, Purity.nsfw]
 
         for purity in all_purity:
-            response = await api.search(purity=purity)
-            wallpapers = response["data"]
-            for wallpaper in wallpapers:
-                self.assertIn(wallpaper["purity"], purity._get_active_names())
+            search_filter = SearchFilter(purity=PurityFilter(purity))
+            response = await api.search(search_filter=search_filter)
+            for wallpaper in response.wallpapers:
+                self.assertEqual(wallpaper.purity, purity)
 
     async def test_sorting_date_added(self):
         target_sorting = Sorting.date_added
         target_order = Order.desc
-        response = await api.search(sorting=target_sorting, order=target_order)
+        search_filter = SearchFilter(sorting=target_sorting, order=target_order)
+        response = await api.search(search_filter=search_filter)
 
-        wallpapers = response["data"]
-        previous_date = get_wallpaper_datetime(wallpapers[0]["created_at"])
-        for wallpaper in wallpapers:
-            current_wallpaper_date = get_wallpaper_datetime(wallpaper["created_at"])
+        previous_date = get_wallpaper_datetime(response.wallpapers[0].created_at)
+        for wallpaper in response.wallpapers:
+            current_wallpaper_date = get_wallpaper_datetime(wallpaper.created_at)
             self.assertLessEqual(current_wallpaper_date, previous_date)
             previous_date = current_wallpaper_date
 
     async def test_sorting_views(self):
         target_sorting = Sorting.views
         target_order = Order.desc
-        response = await api.search(sorting=target_sorting, order=target_order)
-        wallpapers = response["data"]
+        search_filter = SearchFilter(sorting=target_sorting, order=target_order)
+        response = await api.search(search_filter=search_filter)
 
-        previous_views = int(wallpapers[0][target_sorting.value])
-        for wallpaper in wallpapers:
-            current_views = int(wallpaper[target_sorting.value])
+        previous_views = int(response.wallpapers[0].views)
+        for wallpaper in response.wallpapers:
+            current_views = int(wallpaper.views)
             self.assertLessEqual(current_views, previous_views)
             previous_views = current_views
 
     async def test_sorting_random(self):
         target_sorting = Sorting.random
-        result = await api.search(sorting=target_sorting)
-        self.assertIsNotNone(result["meta"]["seed"])  # random set seed
+        search_filter = SearchFilter(sorting=target_sorting)
+        result = await api.search(search_filter=search_filter)
+        self.assertIsNotNone(result.meta.seed)  # random set seed
 
     async def test_sorting_favorites(self):
         target_sorting = Sorting.favorites
         target_order = Order.desc
-        response = await api.search(sorting=target_sorting, order=target_order)
+        search_filter = SearchFilter(sorting=target_sorting, order=target_order)
+        response = await api.search(search_filter=search_filter)
 
-        wallpapers = response["data"]
-        previous_favorites = int(wallpapers[0][target_sorting.value])
-        for wallpaper in wallpapers:
-            current_favorites = int(wallpaper[target_sorting.value])
+        previous_favorites = int(response.wallpapers[0].favorites)
+        for wallpaper in response.wallpapers:
+            current_favorites = int(wallpaper.favorites)
             self.assertLessEqual(current_favorites, previous_favorites)
             previous_favorites = current_favorites
 
     async def test_at_least(self):
         target_at_least = Resolution(3000, 3000)
-        response = await api.search(atleast=target_at_least)
+        search_filter = SearchFilter(atleast=target_at_least)
+        response = await api.search(search_filter=search_filter)
 
-        wallpapers = response["data"]
-        for wallpaper in wallpapers:
-            current_resolution = wallpaper["resolution"].split("x")
-            current_x = current_resolution[0]
-            current_y = current_resolution[1]
+        for wallpaper in response.wallpapers:
+            current_x = wallpaper.dimension_x
+            current_y = wallpaper.dimension_y
             self.assertGreaterEqual(int(current_x), int(target_at_least.x))
             self.assertGreaterEqual(int(current_y), int(target_at_least.y))
 
     async def test_resolution(self):
         target_resolution = [Resolution(1920, 1080)]
-        response = await api.search(resolutions=target_resolution)
+        search_filter = SearchFilter(resolutions=target_resolution)
+        response = await api.search(search_filter=search_filter)
 
-        wallpapers = response["data"]
-        for wallpaper in wallpapers:
-            self.assertEqual(wallpaper["resolution"], str(target_resolution[0]))
+        for wallpaper in response.wallpapers:
+            self.assertEqual(wallpaper.resolution, target_resolution[0])
 
     async def test_ratios(self):
         target_ratio = Ratio(1, 1)
-        response = await api.search(ratios=[target_ratio])
+        search_filter = SearchFilter(ratios=[target_ratio])
+        response = await api.search(search_filter=search_filter)
 
-        wallpapers = response["data"]
-        for wallpaper in wallpapers:
-            self.assertEqual(target_ratio.x / target_ratio.y, float(wallpaper["ratio"]))
+        for wallpaper in response.wallpapers:
+            self.assertEqual(target_ratio.x / target_ratio.y, wallpaper.ratio)
 
     async def test_color(self):
         target_color = Color.black
-        response = await api.search(color=target_color)
-        wallpapers = response["data"]
+        search_filter = SearchFilter(color=target_color)
+        response = await api.search(search_filter=search_filter)
 
-        for wallpaper in wallpapers:
-            self.assertIn("#" + target_color.value, wallpaper["colors"])
+        for wallpaper in response.wallpapers:
+            self.assertIn("#" + target_color.value, wallpaper.colors)
 
     async def test_page(self):
         target_page = 2
-        response = await api.search(q="anime", page=target_page)
-        self.assertEqual(target_page, int(response["meta"]["current_page"]))
+        search_filter = SearchFilter(page=target_page)
+        response = await api.search(query="anime", search_filter=search_filter)
+        self.assertEqual(target_page, int(response.meta.current_page))
 
     # ------------------------------ #
     #      Manual Test Section       #
@@ -156,26 +164,30 @@ class ApiTestSearch(unittest.IsolatedAsyncioTestCase):
 
     async def test_sorting_toplist(self):
         target_sorting = Sorting.toplist
-        response = await api.search(sorting=target_sorting)
-        self.assertIsNotNone(response["data"])
+        search_filter = SearchFilter(sorting=target_sorting)
+        response = await api.search(search_filter=search_filter)
+        self.assertIsNot(response.wallpapers, [])
 
     async def test_sorting_relevance(self):
         target_sorting = Sorting.relevance
-        response = await api.search(sorting=target_sorting)
-        self.assertIsNotNone(response["data"])
+        search_filter = SearchFilter(sorting=target_sorting)
+        response = await api.search(search_filter=search_filter)
+        self.assertIsNot(response.wallpapers, [])
 
     async def test_toprange(self):
         target_toprange = TopRange.one_day
-        response = await api.search("anime", toprange=target_toprange)
-        self.assertIsNotNone(response["data"])
+        search_filter = SearchFilter(toprange=target_toprange)
+        response = await api.search("anime", search_filter=search_filter)
+        self.assertIsNot(response.wallpapers, [])
 
     # Something is completely wrong with seed values
     # it seems that API just ignores it, because different seeds
     # give same page of wallpapers (even with sorting=random)
     async def test_seed(self):
         target_seed = "abc123"
-        response = await api.search(seed=target_seed)
-        self.assertIsNotNone(response["data"])
+        search_filter = SearchFilter(seed=target_seed)
+        response = await api.search(search_filter=search_filter)
+        self.assertIsNot(response.wallpapers, [])
 
 
 class ApiTestGet(unittest.IsolatedAsyncioTestCase):
@@ -190,39 +202,29 @@ class ApiTestGet(unittest.IsolatedAsyncioTestCase):
 
     async def test_get_collections(self):
         username = "Raylz"
-        target_purity = Purity(sfw=True)
-
-        # first - get all collections
-        response = await api.get_collections(username)
-
-        # if there is something in the list - take first collection
-        if response["data"]:
-            collection_id = response["data"][0]["id"]
-            response = await api.get_collections(
-                username, collection_id, purity=target_purity
-            )
-
-            collection_wallpapers = response["data"]
-            for wallpaper in collection_wallpapers:
-                self.assertIn(wallpaper["purity"], target_purity._get_active_names())
+        response = await api.get_user_collections_list(username)
+        for collection in response:
+            self.assertIsInstance(collection, UserCollectionInfo)
 
     async def test_get_tag(self):
-        res = await api.get_tag(1)
-        self.assertIsNotNone(res["data"])
+        tag = await api.get_tag(1)
+        self.assertIsInstance(tag, WallpaperTag)
+        self.assertIsNotNone(tag.name)
 
     async def test_get_settings(self):
-        res = await api.my_settings()
-        self.assertIsNotNone(res["data"])
+        settings = await api.my_settings()
+        self.assertIsInstance(settings, UserSettings)
 
     async def test_get_user_uploads(self):
-        res = await api.get_user_uploads("provip")
-        self.assertIsNotNone(res["data"])
+        uploads_collection = await api.get_user_uploads("provip")
+        self.assertIsInstance(uploads_collection, WallpaperCollection)
 
     async def test_get_wallpaper(self):
         test_wallpaper_id = "e7jj6r"
-        res = await api.get_wallpaper(test_wallpaper_id)
-        self.assertEqual(res["data"]["id"], test_wallpaper_id)
-        self.assertIsNotNone(res["data"])
+        wallpaper = await api.get_wallpaper(test_wallpaper_id)
+        self.assertIsInstance(wallpaper, WallpaperInfo)
+        self.assertEqual(wallpaper.id, test_wallpaper_id)
+        self.assertIsNotNone(wallpaper.path)
 
 
 if __name__ == "__main__":
